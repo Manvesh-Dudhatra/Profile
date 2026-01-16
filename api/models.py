f59590b720c990d53ecffcfbb058f0b1dae5bb2f
Manvesh-Dudhatra <dudhatramanvesh23@gmail.com>
2026-01-16 15:31:04 +0530

    initial_setup_with_signup_page

---
+++ api/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    profile_image = models.ImageField(upload_to = 'account/', null = True, blank = True)
+    mobile = models.CharField(max_length = 10, null = True, blank = True)
+
