b18fbbc0cf41951aa89afa7d96a3e814ad2c03be
Manvesh-Dudhatra <dudhatramanvesh23@gmail.com>
2026-02-17 16:16:52 +0530

    perform authentication

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CustomUser
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = CustomUser(username =username, email = email)
+        user.set_password(password)
+        user.save()
+        return redirect('login')
+
+    return render(request, "auth/register.html")
+
+def userlogin(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username = username, password = password)
+        if not user:
+            return redirect('login')
+
+        login(request, user)
+        return redirect('home')
+    return render(request, 'auth/login.html')
+
+def home(request):
+    return render(request, 'home.html')
+
